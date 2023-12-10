3736b911f15aefdc50aba67cb4c7785f6a610beb
eslupmi101 <ajsen-andreev@yandex.ru>
2023-12-10 13:24:19 +0900

    Add services accounts and load_balancer Add docker compose for local and productin

---
+++ services/accounts/users/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["username", "email", "first_name", "last_name", "is_staff"]
+    list_filter = ["is_staff"]
+    fieldsets = [
+        [None, {"fields": ["email", "password"]}],
+        ["Personal Info", {"fields": [
+            "username", "first_name", "last_name"
+        ]}],
+        ["Permissions", {"fields": [
+            "is_active", "is_staff", "is_superuser",
+            "groups", "user_permissions"
+        ]}],
+        ["Important dates", {"fields": ["last_login", "date_joined"]}],
+    ]
+    add_fieldsets = [
+        [None, {
+            "classes": ["wide"],
+            "fields": ["email", "password1", "password2"],
+        }],
+    ]
+    search_fields = ["email", "username", "first_name", "last_name"]
+    ordering = ["email"]
