9a5cff660148e1488231fabbb850f6e13947b558
eslupmi101 <ajsen-andreev@yandex.ru>
2023-12-10 14:42:53 +0900

    Add app in django settings

--- services/accounts/accounts/settings.py
+++ services/accounts/accounts/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'users.apps.UsersConfig',
     'allauth',
     'allauth.account',
